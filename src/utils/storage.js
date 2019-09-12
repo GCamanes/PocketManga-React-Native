1e66b743842e3cae78d438fbad5973b4b68c186e
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-12 18:06:43 +0200

    # update AsyncStorage to import from react-native-community

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 function setItem(key, value) {
   return new Promise(async (resolve, reject) => {
